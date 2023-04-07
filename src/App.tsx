a51488e8b3f522690f79d263a3979970fd7936db
root <miladgera014@gmail.com>
2023-04-07 18:10:48 +0430

    feat: add windicss for styling system

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
+      <Box className={'bg-red-400'} sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
           Material UI Vite.js example
         </Typography>
--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import 'virtual:windi.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
