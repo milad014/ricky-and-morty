fa138d170f166dbeb5e6639b5684904615af0bcd
root <miladgera014@gmail.com>
2023-04-07 18:10:48 +0430

    refactor(App): removed unneccesseries

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Button from '@mui/material/Button';
 
-import './App.css'
 import { Copyright } from '@mui/icons-material';
 import { Container, Box, Typography } from '@mui/material';
 
