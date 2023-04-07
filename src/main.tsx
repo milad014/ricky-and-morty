efdbc27cbef91ec2fed489e2e35dd4a312c56aed
root <miladgera014@gmail.com>
2023-04-07 18:10:48 +0430

    feat: add redux to project

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,11 @@ import App from './App'
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 
+
+import { Provider } from 'react-redux'
+import { store } from './app/store'
+
+
 import theme from './theme';
 
 import '@fontsource/roboto/300.css';
@@ -16,10 +21,12 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode >,
 )
